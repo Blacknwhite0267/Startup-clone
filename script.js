45c783ab5e431b14c29c0a65b2798fd0e9118cac
Blacknwhite <tobyem19@gmail.com>
2022-07-05 11:50:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('.menu');
+const navbar = document.querySelector('.h2');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+    
+});
